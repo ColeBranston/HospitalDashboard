b77e440ef714d9c02ed494a7c5e33f827b411520
ColeBranston <118498961+ColeBranston@users.noreply.github.com>
2025-01-29 01:42:24 -0500

    added base page with directories and skeleton Nav

---
+++ Frontend/Frontend/src/components/nav.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <div className="mainContainer">
+            <div className="pathContainer">
+                <p>News</p>
+                <p>Booking</p>
+                <p>Schedule</p>
+            </div>
+            <div className="accountContainer">
+                <p>Account</p>
+            </div>
+        </div>
+    );
+};
+
+export default Nav;

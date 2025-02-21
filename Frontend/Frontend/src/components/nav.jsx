a8d96c90fe3aac3843f7739cff2372011dd6fb99
ColeBranston <118498961+ColeBranston@users.noreply.github.com>
2025-02-21 01:20:53 -0500

    fixed nav bar animation problem

--- Frontend/Frontend/src/components/nav.jsx
+++ Frontend/Frontend/src/components/nav.jsx
@@ -10,7 +10,7 @@ const Nav = () => {
             <div className="mainContainer">
                 <div className="pathContainer">
                     <Link to="/" className='navButtons' id="medicalImage"><div><img src={MedicalIcon} alt="Medical Symbol" width="50px" height="50px"/></div></Link>
-                    <Link to="/NewsPage" className='navButtons'><div className='navButtons'><p>News</p></div></Link>
+                    <Link to="/NewsPage" className='navButtons'><div><p>News</p></div></Link>
                     <Link to="/BookingPage" className='navButtons'><div><p>Booking</p></div></Link>
                     <Link to="/SchedulePage" className='navButtons'><div><p>Schedule</p></div></Link>
                 </div>
